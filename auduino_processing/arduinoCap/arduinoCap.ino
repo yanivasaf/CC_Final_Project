767a108de93828de27393700fad630f51ca28782
yanivasaf <asafyaniv@Asafs-MacBook-Pro.local>
2017-05-04 19:19:40 -0400

    Changing capacity in the arduino

--- auduino_processing/arduinoCap/arduinoCap.ino
+++ auduino_processing/arduinoCap/arduinoCap.ino
@@ -18,30 +18,30 @@ void setup()
 void loop()                    
 {
     //create variables for each trigger
-    long trigger1 =  cs_2_4.capacitiveSensor(20);
-    long trigger2 =  cs_2_7.capacitiveSensor(10);
-    long trigger3 =  cs_2_8.capacitiveSensor(20);
-    long trigger4 =  cs_2_12.capacitiveSensor(20);
-    long trigger5=  cs_2_13.capacitiveSensor(20);
+    long trigger1 =  cs_2_4.capacitiveSensor(120);
+    long trigger2 =  cs_2_7.capacitiveSensor(120);
+    long trigger3 =  cs_2_8.capacitiveSensor(120);
+    long trigger4 =  cs_2_12.capacitiveSensor(120);
+    long trigger5=  cs_2_13.capacitiveSensor(120);
     
 
    //printing output for each trigger 
-    if (trigger1 > 5) {
+    if (trigger1 > 4) {
         Serial.println('1');
     } 
-     if (trigger2 > 5) {
+     if (trigger2 > 4) {
         Serial.println('2');
     } 
-     if (trigger3 > 5) {
+     if (trigger3 > 4) {
         Serial.println('3');
     } 
-     if (trigger4 > 5) {
+     if (trigger4 >4) {
         Serial.println('4');
     } 
-     if (trigger5 > 5) {
+     if (trigger5 > 4) {
         Serial.println('5');
     } 
-    if (trigger1 < 5 && trigger2 < 5 && trigger3 < 5 && trigger4 < 5 && trigger5 < 5){
+    if (trigger1 < 4 && trigger2 < 5 && trigger3 < 5 && trigger4 < 5 && trigger5 < 5){
         Serial.println('0');
     }
     
